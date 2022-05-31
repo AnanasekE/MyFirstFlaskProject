d456e3e3f9dc4e8c3478290d94616a89b227c3e7
AnanasekE <janeksambor@gmail.com>
2022-05-31 16:07:55 +0200

    commit

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
-import sqlalchemy as sql
+from flask import Flask, render_template, request
+import yaml
+from yaml import Loader
 
 app = Flask(__name__)
 
 
+def routeFinder():
+    return 'SUS'
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    with open('config.yaml') as f:
+        config = yaml.load(f, Loader=Loader)
+    print(config)
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
